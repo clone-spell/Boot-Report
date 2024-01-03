87aa24943809607f7b3dfab3c2884811ed3f8725
Bablu Shaikh <bablushaikh0000@outlook.com>
2024-01-03 15:08:39 +0530

    Add project files.

---
+++ Boot Report/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Boot_Report
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblGenerating = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Month :";
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(59, 13);
+            this.txtMonth.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(184, 25);
+            this.txtMonth.TabIndex = 0;
+            this.txtMonth.Enter += new System.EventHandler(this.txtMonth_Enter);
+            this.txtMonth.Leave += new System.EventHandler(this.txtMonth_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Year :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Report :";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 433);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(464, 10);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(13, 81);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(465, 345);
+            this.txtOutput.TabIndex = 3;
+            this.txtOutput.TabStop = false;
+            this.txtOutput.Text = "";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(384, 477);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(93, 51);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(285, 477);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(93, 51);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblGenerating
+            // 
+            this.lblGenerating.AutoSize = true;
+            this.lblGenerating.Location = new System.Drawing.Point(12, 449);
+            this.lblGenerating.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGenerating.Name = "lblGenerating";
+            this.lblGenerating.Size = new System.Drawing.Size(0, 17);
+            this.lblGenerating.TabIndex = 0;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // cbYear
+            // 
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(296, 13);
+            this.cbYear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(179, 25);
+            this.cbYear.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 549);
+            this.Controls.Add(this.cbYear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblGenerating);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(506, 588);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(506, 588);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Show Boot Time v4";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblGenerating;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ComboBox cbYear;
+    }
+}
+
---
+++ Boot Report/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Boot_Report
+{
+    public partial class Form1 : Form
+    {
+        public List<string> result = new List<string>();
+        public int count = 0;
+        public DateTime expiryDate;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //expiry date
+            expiryDate = new DateTime(2024,03,31);
+
+
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+
+            List<int> years = new List<int>();
+            int currentYear = DateTime.Now.Year;
+            for(int i = currentYear-2; i < currentYear+3; i++)
+            {
+                years.Add(i);
+            }
+
+            cbYear.DataSource = years;
+            cbYear.SelectedItem = currentYear;
+            txtMonth.Text = "In 'mm' format";
+            txtMonth.ForeColor = SystemColors.GrayText;
+
+            this.AcceptButton = btnGenerate;
+        }
+
+        struct DataParameter
+        {
+            public string month;
+            public string year;
+        }
+
+        private DataParameter _inputParameter;
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string month = _inputParameter.month;
+            string year = _inputParameter.year;
+            string day;
+            int noOfDays = GetDaysInMonth(month);
+
+            string filePath;
+
+            for (int i = 1; i <= noOfDays; i++)
+            {
+                day = i.ToString("D2");
+                filePath = $"C:\\V_Services\\data\\{year}{month}{day}\\Logfile.log";
+
+
+                // Perform some work here...
+                result.Add(GetFirstAndLastLineChars(filePath));
+
+                Thread.Sleep(15);
+                // Report the progress to the UI thread
+                int progressPercentage = (int)((double)i / noOfDays * 100);
+                backgroundWorker.ReportProgress(progressPercentage);
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblGenerating.Text = "Generating....." + e.ProgressPercentage.ToString()+"%";
+            txtOutput.AppendText(result[count] + Environment.NewLine);
+            count++;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblGenerating.Text = "Generated.....100%";
+            btnGenerate.Enabled = true;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //check input
+            try
+            {
+                if (txtMonth.Text == "" || cbYear.Text == "" || int.Parse(txtMonth.Text) > 12 || int.Parse(txtMonth.Text) <= 0 || int.Parse(cbYear.Text) > DateTime.Now.Year)
+                {
+                    MessageBox.Show("Enter valid input", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Enter valid input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+
+
+            btnGenerate.Enabled = false;
+
+            if (!backgroundWorker.IsBusy)
+            {
+                _inputParameter.month = txtMonth.Text;
+                _inputParameter.year = cbYear.Text;
+
+
+                backgroundWorker.RunWorkerAsync(_inputParameter);
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string txtToCopy = txtOutput.Text;
+            if(txtToCopy == "")
+            {
+                MessageBox.Show("Nothing to copy here","Empty Clipboard",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    Clipboard.SetText(txtToCopy);
+                    MessageBox.Show("Copied succesfuly", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Copied succesfuly", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+        //methods
+        //get days in month
+        public static int GetDaysInMonth(string month)
+        {
+            int daysInMonth = 0;
+
+            if (int.TryParse(month, out int monthNumber))
+            {
+                if (monthNumber >= 1 && monthNumber <= 12)
+                {
+                    switch (monthNumber)
+                    {
+                        case 1: // January
+                        case 3: // March
+                        case 5: // May
+                        case 7: // July
+                        case 8: // August
+                        case 10: // October
+                        case 12: // December
+                            daysInMonth = 31;
+                            break;
+                        case 4: // April
+                        case 6: // June
+                        case 9: // September
+                        case 11: // November
+                            daysInMonth = 30;
+                            break;
+                        case 2: // February
+                            int year = DateTime.Now.Year; // Use the current year for simplicity
+                            daysInMonth = DateTime.IsLeapYear(year) ? 29 : 28;
+                            break;
+                    }
+                }
+            }
+
+            return daysInMonth;
+        }
+
+
+        //geting times
+        public static string GetFirstAndLastLineChars(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return "not found\tnot found";
+
+            string firstChars = "";
+            string lastChars = "";
+
+            try
+            {
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Get the first line and its first 8 characters
+                if (lines.Length > 0)
+                {
+                    for(int i = 0; i < lines.Length; i++)
+                    {
+                        string firstLine = lines[i];
+                        firstChars = firstLine.Length >= 8 ? firstLine.Substring(0, 8) : firstLine;
+                        if (firstChars.Contains(":") && firstChars.Length == 8 && !firstChars.Contains(" ") && !firstChars.Contains("\t"))
+                            break;
+                    }
+                }
+
+                // Get the last line and its first 8 characters
+                if (lines.Length > 1)
+                {
+                    for (int i = lines.Length - 1; i >= 0; i--)
+                    {
+                        string lastLine = lines[i];
+                        lastChars = lastLine.Length >= 8 ? lastLine.Substring(0, 8) : lastLine;
+                        if (lastChars.Contains(":") && lastChars.Length == 8 && !lastChars.Contains(" ") && !lastChars.Contains("\t"))
+                            break;
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle file reading errors, if any
+                Console.WriteLine($"Error reading the file: {ex.Message}");
+            }
+
+
+            if (!(firstChars.Contains(":") && firstChars.Length == 8 && !firstChars.Contains(" ") && !firstChars.Contains("\t")) || !(lastChars.Contains(":") && lastChars.Length == 8 && !lastChars.Contains(" ") && !lastChars.Contains("\t")))
+            {
+                return "not found\tnot found";
+            }
+
+            
+            try
+            {
+                TimeSpan duration = DateTime.Parse(lastChars) - DateTime.Parse(firstChars);
+                return firstChars +"\t"+ lastChars + "\t" + duration.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            // Concatenate the first 8 characters of the first and last lines
+            return firstChars +"\t"+ lastChars + "\t" +"";
+        }
+
+        private void txtMonth_Enter(object sender, EventArgs e)
+        {
+            if (txtMonth.Text == "In 'mm' format")
+            {
+                txtMonth.Text = string.Empty;
+                txtMonth.ForeColor = SystemColors.MenuText;
+            }
+        }
+
+        private void txtMonth_Leave(object sender, EventArgs e)
+        {
+            if (txtMonth.Text == string.Empty)
+            {
+                txtMonth.Text = "In 'mm' format";
+                txtMonth.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            TimeSpan expiringIn = expiryDate - DateTime.Now;
+            MessageBox.Show($"Lisence Expiring in {expiringIn.Days}d {expiringIn.Hours}h {expiringIn.Minutes}m {expiringIn.Seconds}s\nFor any help contact me at bablushaikh0000@gmail.com","About",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (expiryDate <  DateTime.Now)
+            {
+                MessageBox.Show("Your Application is Expired.\nPlease Contact Administrator","Hello",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                this.Close();
+            }
+
+        }
+    }
+}
